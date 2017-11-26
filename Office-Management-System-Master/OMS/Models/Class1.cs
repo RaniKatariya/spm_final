4a45db3f0fbb20ba25a50ca698d4f65af8071005
Rani katariya <katariyarani68@gmail.com>
2017-11-26 14:17:19 +0530

    Class2 commit through vs

--- Office-Management-System-Master/OMS/Models/Class1.cs
+++ Office-Management-System-Master/OMS/Models/Class1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -13,5 +15,17 @@ namespace OMS.Models
         public int age { get; set; }
         public string email { get; set; }
         public int password { get; set; }
+    }
+    public class Class2
+    {
+        [Key]
+        public int sid { get; set; }
+        public string tname { get; set; }
+        public int course { get; set; }
+        public int age { get; set; }
+        public string email { get; set; }
+        public int password { get; set; }
+        [ForeignKey("id")]
+        public int id { get; set; }
     }
 }
