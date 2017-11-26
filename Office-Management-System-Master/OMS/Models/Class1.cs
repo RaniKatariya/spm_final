cb8952408a90898fde756fbdb7e8984b7dab778a
Rani katariya <katariyarani68@gmail.com>
2017-11-26 14:09:40 +0530

    Class1 change through vs

--- Office-Management-System-Master/OMS/Models/Class1.cs
+++ Office-Management-System-Master/OMS/Models/Class1.cs
@@ -1,1 +1,17 @@
-Class1
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OMS.Models
+{
+    public class Class1
+    {
+        public int id { get; set; }
+        public string sname { get; set; }
+        public int course { get; set; }
+        public int age { get; set; }
+        public string email { get; set; }
+        public int password { get; set; }
+    }
+}
