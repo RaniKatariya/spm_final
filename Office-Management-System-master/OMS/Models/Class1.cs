b9b1b943fed43a870c60a5d1ee1473991cd7784f
Rani katariya <katariyarani68@gmail.com>
2017-11-27 09:47:38 +0530

    very

--- Office-Management-System-master/OMS/Models/Class1.cs
+++ Office-Management-System-master/OMS/Models/Class1.cs
@@ -1,1 +1,19 @@
-hello
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OMS.Models
+{
+    public class Class1
+    {
+        public  int id { get; set; }
+        public string sname { get; set; }
+        public string  course { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int mobileNo{ get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
